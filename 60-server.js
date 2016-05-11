e805f3044485170e4c65b2ebe228c1986e1cc0b5
Michael McCartney <m.a.mccartney@gmail.com>
2016-05-11 10:20:02 -0500

    Add PUT /todos/:id route

---
+++ 60-server.js
@@ -0,0 +1,101 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.send('Todo API Root');
+});
+
+app.get('/todos', function (req, res) {
+  res.json(todos);
+});
+
+app.get('/todos/:id', function (req, res) {
+  // Number() or parseInt() will do...
+  var id = parseInt(req.params.id);
+  // use underscore function _.findWhere()
+  var todo = _.findWhere(todos, {id: id});
+
+  if (todo) {
+    res.json(todo);
+  }
+  else {
+    // res.status(404).send('id: ' + req.params.id + ' not found');
+    res.status(404).send();
+  }
+});
+
+// process incoming json, add to todos array
+app.post('/todos', function (req, res) {
+  var body = _.pick(req.body, 'description', 'completed');
+
+  if (!_.isBoolean(body.completed) ||
+      !_.isString(body.description) ||
+      body.description.trim().length === 0) {
+        return res.status(400).send()
+  }
+
+  body.description = body.description.trim();
+  body.id = todoNextId++;
+
+  todos.push(body);
+  res.json(body);
+});
+
+app.delete('/todo/:id', function (req, res) {
+  var id = parseInt(req.params.id, 10);
+  var todo = _.findWhere(todos, {id: id});
+  // console.log(id, todo);
+  if (todo) {
+    todos = _.without(todos, todo);
+    res.json(todo);
+  }
+  else {
+    res.status(404).json({"error": "no todo found with that id"});
+  }
+});
+
+app.put('/todos/:id', function (req, res) {
+  var id = parseInt(req.params.id, 10);
+  var todo = _.findWhere(todos, {id: id});
+
+  if (!todo) {
+    return res.status(404).send();
+  }
+
+  var body = _.pick(req.body, 'description', 'completed');
+  var validAttributes = {};
+
+  if (body.hasOwnProperty('completed') &&
+      _.isBoolean(body.completed)) {
+    validAttributes.completed = body.completed;
+  }
+  else if (body.hasOwnProperty('completed')) {
+    return res.status(400).send();
+  }
+
+  if (body.hasOwnProperty('description') &&
+      _.isString(body.description) &&
+      body.description.trim().length > 0) {
+    validAttributes.description = body.description.trim();
+  }
+  else if (body.hasOwnProperty('description')) {
+    return res.status(400).send();
+  }
+
+  // apparently todo is like a ptr to todos,
+  _.extend(todo, validAttributes);
+  res.json(todo);
+
+});
+
+app.listen(PORT, function () {
+  console.log('Express listening on port ' + PORT);
+});
